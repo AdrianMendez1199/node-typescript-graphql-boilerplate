e8ebdfc8b0ddc972315d4302049e33e119bee051
Adrian Mendez <mendezadrian149@gmail.com>
2020-08-01 10:54:53 -0400

    adding typeorm to project

--- index.ts
+++ index.ts
@@ -1,11 +1,33 @@
 import { ApolloServer } from 'apollo-server';
+import { createConnection } from 'typeorm';
+import 'reflect-metadata';
 import 'module-alias/register';
 import { typeDefs, resolvers } from '@api/graphql';
 import AuthDirective from '@api/graphql/directives/Authentication';
 
 
+// Db Connection on start server
+(async (): Promise<void> => {
+  try {
+    await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'typeorm2',
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/entity/*{.js,.ts}'],
+      extra: { max: 10, min: 1 },
+    })
+  } catch (error) {
+    process.exit(0);
+  }
+})();
+
 const server = new ApolloServer({
-  typeDefs, 
+  typeDefs,
   resolvers,
   schemaDirectives: { AuthDirective }
 });
