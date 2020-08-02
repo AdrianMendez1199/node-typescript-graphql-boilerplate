56cc2bc51fe804b24713378f8dff8bfaf7250e71
Adrian Mendez <mendezadrian149@gmail.com>
2020-08-01 21:13:31 -0400

    addind env variables to config db connection

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from 'apollo-server';
 import { createConnection } from 'typeorm';
 import 'reflect-metadata';
 import 'module-alias/register';
+import 'dotenv/config';
 import { typeDefs, resolvers } from '@api/graphql';
 import AuthDirective from '@api/graphql/directives/Authentication';
 
@@ -11,13 +12,13 @@ import AuthDirective from '@api/graphql/directives/Authentication';
   try {
     await createConnection({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'typeorm2',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       synchronize: true,
-      logging: true,
+      logging: false,
       entities: [__dirname + '/entity/*{.js,.ts}'],
       extra: { max: 10, min: 1 },
     })
@@ -29,10 +30,12 @@ import AuthDirective from '@api/graphql/directives/Authentication';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  schemaDirectives: { AuthDirective }
+  schemaDirectives: { AuthDirective },
 });
 
 
 server.listen().then(({ url }): void => {
+  /* eslint-disable no-console */
   console.log(`Server runing on ${url}`);
+  /* eslint-disable no-console */
 })
