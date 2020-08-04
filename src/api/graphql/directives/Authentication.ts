4685ff079e181ed554b1577862184659066f0b34
Adrian Mendez <mendezadrian149@gmail.com>
2020-08-03 22:27:37 -0400

    adding decode token to context

--- src/api/graphql/directives/Authentication.ts
+++ src/api/graphql/directives/Authentication.ts
@@ -1,6 +1,6 @@
 import { AuthenticationError, SchemaDirectiveVisitor } from 'apollo-server';
 import { defaultFieldResolver, GraphQLField } from 'graphql';
-
+import { getToken } from '@utils/jwt';
 
 export default class AuthDirective extends SchemaDirectiveVisitor {
 
@@ -20,6 +20,10 @@ export default class AuthDirective extends SchemaDirectiveVisitor {
       if (!context.request.req.headers.authorization) {
         throw new AuthenticationError('Not authenticate');
       }
+
+      // adding decode token to context
+      context.request.auth = getToken(context.request.req.headers.authorization);
+
       return await originalResolve.apply(this, args);
     };
   }
