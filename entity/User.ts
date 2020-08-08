5ec40450c214c5f0d20b89de1a621da4f811333d
Adrian Mendez <mendezadrian149@gmail.com>
2020-08-07 21:36:08 -0400

    working on add username and password to entity user

--- entity/User.ts
+++ entity/User.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BaseEntity,
+    BeforeInsert
+} from 'typeorm';
+import bcrypt from 'bcrypt';
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,9 +17,23 @@ export class User extends BaseEntity {
     firstName: string;
 
     @Column()
+    username: string;
+
+
+    @Column()
     lastName: string;
 
     @Column()
     age: number;
+
+
+    @Column()
+    password: string;
+
+
+    @BeforeInsert()
+    async hashPasswordBeforeInsert(): Promise<void> {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
 
 }
